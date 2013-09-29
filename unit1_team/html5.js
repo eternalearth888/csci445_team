f6ea8497feb232b596b810d530b1a28a0c641987
Nicola Hetrick <nhetrick@mines.edu>
2013-09-29 12:11:11 -0600

    Completed 2 Pieces

--- unit1_team/html5.js
+++ unit1_team/html5.js
@@ -4,9 +4,38 @@ var numRows = 18;
 var direction = 0;
 var x = 0;
 var y = 0;
+var colors = ["green", "orange", "yellow", "red", "blue", "cyan", "purple"];
+var count = 4;
 
+//T 
+var T = [[2,[0, -1, -1, -1],[0, 0, 1, -1]],[2,[0, -1, -1, -2],[0, -1, 0, 0]],
+		[2,[0, 0, 0, -1],[0, -1, 1, 0]],[2,[0, -1, -1, -2],[0, 0, 1, 0]]];
+var uT = [[1,[-2, -2, -2],[0, 1, -1]],[2,[-3, -2],[0, -1]],
+		[1,[-1, -1, -2],[-1, 1, 0]],[2,[-3, -2],[0, 1]]];		
+var rT = [[[-1],[1]],[[-1, -1, -2],[0, -1, 0]],[[0, 0],[-1, 1]],[[-2],[0]]];
+var rightT = [[[-1, 0],[-2, -1]],[[-2, -1, 0],[-1, -2, -1]],
+		[[0, -1],[-2, -1]],[[-2, -1, 0],[-1, -1, -1]]];
+var leftT = [[[-1, 0],[2, 1]],[[-2, -1, 0],[1, 1, 1]],
+		[[0, -1],[2, 1]],[[-2, -1, 0],[1, 2, 1]]];
+var sidesT = [[-1, 1, -1, 0],[-1, 0, -1, 1],[-1, 1, -1, 0],[0, 1, -1, 1]];
+	
+//O	
+var O = [[2,[0, -1, -1, 0],[0, -1, 0, -1]],[2,[0, -1, -1, 0],[0, -1, 0, -1]],
+		[2,[0, -1, -1, 0],[0, -1, 0, -1]],[2,[0, -1, -1, 0],[0, -1, 0, -1]]];
+var uO = [[2,[-2, -2],[-1, 0]],[2,[-2, -2],[-1, 0]],
+		[2,[-2, -2],[-1, 0]],[2,[-2, -2],[-1, 0]]];
+var rO = [[[],[]],[[],[]],[[],[]],[[],[]]];
+var rightO = [[[-1, 0],[-2, -2]],[[-1, 0],[-2, -2]],
+		[[-1, 0],[-2, -2]],[[-1, 0],[-2, -2]]];
+var leftO = [[[-1, 0],[1, 1]],[[-1, 0],[1, 1]],
+		[[-1, 0],[1, 1]],[[-1, 0],[1, 1]]];
+var sidesO = [[-1, 0, -1, 0],[-1, 0, -1, 0],[-1, 0, -1, 0],[-1, 0, -1, 0]];
+	
+//S	
+var S = [[2,[0, -1, 0, -1],[0, 0, -1, 1]],[2,[0, -1, -1, -2],[0, 0, -1, -1]],
+		[2,[0, -1, 0, -1],[0, 0, -1, 1]],[2,[0, -1, -1, -2],[0, 0, -1, -1]]];
 
-var KEY = { RIGHT: 39 };
+var KEY = { LEFT: 37, UP: 38, RIGHT: 39 };
 
 window.onload = function(){
 
@@ -32,13 +61,13 @@ window.onload = function(){
 }
 
 function beginGame() {
-	var i=0;
+	var i=2;
+	y=5;
 	var interval = setInterval(function () {
 		x = i;
-		y = 4;
-		drawT(); 
+		drawPiece(O); 
 		i++; 
-		undrawT();
+		undrawPiece(uO);
 		if (i > 17) clearInterval(interval);}, 500);
 }
 
@@ -54,7 +83,7 @@ function draw(row, col) {
 	var i = getID(row, col);
 	var c = document.getElementById("id"+i.toString());
 	var ctx = c.getContext('2d');
-	ctx.fillStyle='red';
+	ctx.fillStyle=colors[count];
 	ctx.fillRect(0,0,size-3,size);
 }
 
@@ -66,88 +95,62 @@ function undraw(row, col) {
 	ctx.fillRect(0,0,size-3,size);
 }
 
-function undrawT() {
-	if (direction == 0) {
-		if (x > 1) {
-			undraw(x-2, y);
-			undraw(x-2, y+1);
-			undraw(x-2, y-1);
-		}
-	} else if (direction == 1) {
-		if (x > 2) {
-			undraw(x-3, y);
-			undraw(x-2, y-1);
-		}
-	} else if (direction == 2) {
-		if (x > 1) {
-			undraw(x-1, y-1);
-			undraw(x-1, y+1);
-			undraw(x-2, y);
-		}
-	} else if (direction == 3) {
-		if (x > 2) {
-			undraw(x-3, y);
-			undraw(x-2, y+1);
+function undrawPiece(piece) {
+	if (x > piece[direction][0]) {
+		for (i=0;i<piece[direction][1].length;i++) {
+			undraw(x + piece[direction][1][i], y + piece[direction][2][i]);
 		}
 	}
 }
 
-function drawT() {
-	if (direction == 0) {
-		if (x >= 2) {
-			draw(x, y);
-			draw(x-1, y);
-			draw(x-1, y+1);
-			draw(x-1, y-1);
-		}
-	} else if (direction == 1) {
-		if (x >= 2) {
-			draw(x, y);
-			draw(x-1, y-1);
-			draw(x-1, y);
-			draw(x-2, y);
-		}
-	} else if (direction == 2) {
-		if (x >= 2) {
-			draw(x, y);
-			draw(x, y-1);
-			draw(x, y+1);
-			draw(x-1, y);
-		}
-	} else if (direction == 3) {
-		if (x >= 2) {
-			draw(x, y);
-			draw(x-1, y);
-			draw(x-1, y+1);
-			draw(x-2, y);
-		}
+function drawPiece(piece) {
+	if (x >= piece[direction][0]) { 
+		draw(x + piece[direction][1][0], y + piece[direction][2][0]);
+		draw(x + piece[direction][1][1], y + piece[direction][2][1]);
+		draw(x + piece[direction][1][2], y + piece[direction][2][2]);
+		draw(x + piece[direction][1][3], y + piece[direction][2][3]);
 	}
 }
 
+function movePieceRight(Mpiece, right, piece) {
+	if (y + Mpiece[direction][1] < 10) {
+		y++;
+	}
+	for (i=0;i<right[direction][0].length;i++) {
+		undraw(x + right[direction][0][i], y + right[direction][1][i]);
+	}
+	drawPiece(piece);
+}
 
+function movePieceLeft(Mpiece, left, piece) {
+	if (y + Mpiece[direction][0] > 1) {
+		y--;
+	}
+	for (i=0;i<left[direction][0].length;i++) {
+		undraw(x + left[direction][0][i], y + left[direction][1][i]);
+	}
+	drawPiece(piece);
+}
+
+function rotatePiece(Rpiece, piece) {
+	for (i=0;i<Rpiece[direction][0].length;i++) {
+		undraw(x + Rpiece[direction][0][i], y + Rpiece[direction][1][i]);
+	}
+	if (direction == 3) {
+		direction = 0;
+	} else {
+		direction++;
+	}
+	drawPiece(piece);
+}
 
 function keydown(ev) {
-	if (ev.keyCode == KEY.RIGHT) {
-		if (direction == 0) {
-			undraw(x-1, y+1);
-			direction = 1;
-			drawT();
-		} else if (direction == 1) {
-			undraw(x-1, y);
-			undraw(x-1,y-1);
-			undraw(x-2,y);
-			direction = 2;
-			drawT();
-		} else if (direction == 2) {
-			undraw(x, y-1);
-			undraw(x, y+1);
-			direction = 3;
-			drawT();
-		} else if (direction == 3) {
-			undraw(x-2, y);
-			direction = 0;
-			drawT();
-		}
+	if (ev.keyCode == KEY.UP) {
+		rotatePiece(rO, O);
+	} else if (ev.keyCode == KEY.RIGHT) {
+		movePieceRight(sidesO, rightO, O);
+	} else if (ev.keyCode == KEY.LEFT) {
+		movePieceLeft(sidesO, leftO, O);
 	}
 }
 
